33bb2d5679c9352e340946fe86d03d58554810a8
evaaldo <evaaldormj@gmail.com>
2023-01-04 14:02:53 -0300

    feat: mudando cor do botao do form e adicionando mais cards para ter melhor noção do posicionamento

--- src/Components/Treinos/index.js
+++ src/Components/Treinos/index.js
@@ -21,6 +21,45 @@ export default function Treinos() {
                     </ul>
                     <BotaoTreinos className='treinos__botao-remover'/>
                 </div>
+                <div className='treinos-container'>
+                    <h2 className='treinos-container__treino'>Treino de peito</h2>
+                    <img className='treinos-container__imagem' src={Peito} alt='Peito'/>
+                    <h4 className='treinos-container__data'>Segunda-feira</h4>
+                    <ul className='treinos-container__lista'>
+                        <li className='treinos-container__item'>Supino reto</li>
+                        <li className='treinos-container__item'>Supino inclinado</li>
+                        <li className='treinos-container__item'>Peck deck</li>
+                        <li className='treinos-container__item'>Pull over</li>
+                        <li className='treinos-container__item'>Crucifixo</li>
+                    </ul>
+                    <BotaoTreinos className='treinos__botao-remover'/>
+                </div>
+                <div className='treinos-container'>
+                    <h2 className='treinos-container__treino'>Treino de peito</h2>
+                    <img className='treinos-container__imagem' src={Peito} alt='Peito'/>
+                    <h4 className='treinos-container__data'>Segunda-feira</h4>
+                    <ul className='treinos-container__lista'>
+                        <li className='treinos-container__item'>Supino reto</li>
+                        <li className='treinos-container__item'>Supino inclinado</li>
+                        <li className='treinos-container__item'>Peck deck</li>
+                        <li className='treinos-container__item'>Pull over</li>
+                        <li className='treinos-container__item'>Crucifixo</li>
+                    </ul>
+                    <BotaoTreinos className='treinos__botao-remover'/>
+                </div>
+                <div className='treinos-container'>
+                    <h2 className='treinos-container__treino'>Treino de peito</h2>
+                    <img className='treinos-container__imagem' src={Peito} alt='Peito'/>
+                    <h4 className='treinos-container__data'>Segunda-feira</h4>
+                    <ul className='treinos-container__lista'>
+                        <li className='treinos-container__item'>Supino reto</li>
+                        <li className='treinos-container__item'>Supino inclinado</li>
+                        <li className='treinos-container__item'>Peck deck</li>
+                        <li className='treinos-container__item'>Pull over</li>
+                        <li className='treinos-container__item'>Crucifixo</li>
+                    </ul>
+                    <BotaoTreinos className='treinos__botao-remover'/>
+                </div>
             </div>
         </>
     )
